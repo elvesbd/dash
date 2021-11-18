971601f1a69d0563109c32fd8badfd36e9628490
elvesbd <elvesbd@yahoo.com>
2021-11-18 19:40:37 -0300

    chore: responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
+     {showProfileData && (
       <Box mr="4" textAlign="right">
         <Text>Elves Brito</Text>
         <Text color="gray.300" fontSize="small">
           elvesbd@dash.com
         </Text>
       </Box>
+     )}
 
       <Avatar size="md" name="Elves Brito" src="https://github.com/elvesbd.png"/>
     </Flex>
